94c6bd6e744ef1797be569ba16aa536905efdf1f
guoliim <showerdown2012@gmail.com>
2021-11-29 21:25:56 +0800

    display header title

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -24,7 +24,7 @@ const LayoutWrapper = ({ children }: Props) => {
                   <Logo width={54} height={44} />
                 </div> */}
                 {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-semibold sm:block">
+                  <div className="h-6 text-2xl font-semibold sm:block">
                     {siteMetadata.headerTitle}
                   </div>
                 ) : (
