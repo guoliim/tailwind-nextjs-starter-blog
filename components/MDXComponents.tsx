738e249c1b9cc618c6ad95772552521ccfbf5259
guoliim <showerdown2012@gmail.com>
2023-01-08 23:19:18 +0800

    update dependencies

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,18 +1,34 @@
 /* eslint-disable react/display-name */
 import React, { useMemo } from 'react'
-import { ComponentMap, getMDXComponent } from 'mdx-bundler/client'
+import { getMDXComponent } from 'mdx-bundler/client'
 import Image from './Image'
 import CustomLink from './Link'
 import TOCInline from './TOCInline'
 import Pre from './Pre'
 import { BlogNewsletterForm } from './NewsletterForm'
+import PostLayout from '@/layouts/PostLayout'
+import AuthorLayout from '@/layouts/AuthorLayout'
+import ListLayout from '@/layouts/ListLayout'
+import PostSimple from '@/layouts/PostSimple'
 
 const Wrapper: React.ComponentType<{ layout: string }> = ({ layout, ...rest }) => {
-  const Layout = require(`../layouts/${layout}`).default
-  return <Layout {...rest} />
+  switch (layout) {
+    case 'PostLayout':
+      // @ts-ignore
+      return <PostLayout {...rest} />
+    case 'AuthorLayout':
+      // @ts-ignore
+      return <AuthorLayout {...rest} />
+    case 'ListLayout':
+      // @ts-ignore
+      return <ListLayout {...rest} />
+    case 'PostSimple':
+      // @ts-ignore
+      return <PostSimple {...rest} />
+  }
 }
 
-export const MDXComponents: ComponentMap = {
+export const MDXComponents = {
   Image,
   //@ts-ignore
   TOCInline,
