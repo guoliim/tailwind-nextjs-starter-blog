c10298bb7b48c90ce73823810d59ec36a8145c3d
guoliim <showerdown2012@gmail.com>
2025-11-08 09:45:52 +0800

    Fix build issues after upstream merge
    - Add RSS icon support to social-icons component
    - Fix image-size API change (v2.x requires Buffer instead of path string)
    - Fix remark-img-to-jsx expression syntax
    - Add logo.svg from upstream
    - Disable strict ESLint rules for require imports
    - Remove deprecated ClientReload component

--- components/social-icons/index.tsx
+++ components/social-icons/index.tsx
@@ -11,6 +11,7 @@ import {
   Instagram,
   Medium,
   Bluesky,
+  RSS,
 } from './icons'
 
 const components = {
@@ -26,6 +27,7 @@ const components = {
   instagram: Instagram,
   medium: Medium,
   bluesky: Bluesky,
+  rss: RSS,
 }
 
 type SocialIconProps = {
